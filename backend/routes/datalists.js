22cd876e64345c85652fcb48f4ec01176f4b1ac2
Irfan <imirfansari@gmail.com>
2023-08-20 12:19:49 +0530

    search implemented

--- backend/routes/datalists.js
+++ backend/routes/datalists.js
@@ -4,4 +4,4 @@ const router =express.Router();
 const getList= require('../controller/datalist');
 
 router.route('/').get(getList);
-module.exports = router ;
+module.exports = router ;// isko ni daalne  se error aa raha tha
